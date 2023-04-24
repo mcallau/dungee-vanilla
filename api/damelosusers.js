d7edcadae41efefab0b355093eff8936209b026b
Gerard Falco <gerardfp@users.noreply.github.com>
2023-04-24 16:42:24 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,6 +1,7 @@
 export default function handler(request, response) {
 
-    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/`)
-    .then(eljsonperocomstring => response.send(eljsonperocomstring))
+    response.send("holaaaa")
+//    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/`)
+//    .then(eljsonperocomstring => response.send(eljsonperocomstring))
 }
 
