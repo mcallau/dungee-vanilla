9c98af044f28a06dd796732da326c6448e994e77
gerard <gerard>
2023-04-25 12:37:12 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,7 +1,9 @@
 export default function handler(request, response) {
 
 
-    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/`)
+    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/`,{
+        method: "GET"
+    })
     .then(eljsonperocomstring => {
        
         response.send(eljsonperocomstring);
