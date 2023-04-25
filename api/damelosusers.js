f28ce78a6e16a55c86e0cd0e5b26f8741f091351
Gerard Falco <gerardfp@users.noreply.github.com>
2023-04-25 15:24:14 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,6 +1,6 @@
 export default function handler(request, response) {
 
-    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins/PIN${pinEl.value}.json`)
+    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins.json`)
     .then(data => data.json())
     .then(json => response.json(json))
 }
