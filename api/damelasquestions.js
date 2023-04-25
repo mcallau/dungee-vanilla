c05635f477aa91922be56559ef198ff1f43f29ae
gerard <gerard>
2023-04-25 13:13:04 +0200

    .

---
+++ api/damelasquestions.js
@@ -0,0 +1,28 @@
+export default function handler(request, response) {
+
+    const questions = 
+        [{
+            title: "Quant es u mes u?",
+            answers: [
+                "un","dos","tres,","quatre"
+            ],
+            correct: 1
+        },
+        {
+            title: "Quant es u mes dos?",
+            answers: [
+                "un","dos","tres,","quatre"
+            ],
+            correct: 2
+        },
+        {
+            title: "Quant es dos mes dos?",
+            answers: [
+                "un","dos","tres,","quatre"
+            ],
+            correct: 3
+        }];
+    
+
+    response.json(questions);
+}
