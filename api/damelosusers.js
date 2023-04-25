f6f0998af9f775bbf08f5bec11bf3cdb630495b3
gerard <gerard>
2023-04-25 12:48:37 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,7 +1,7 @@
 export default function handler(request, response) {
 
     const { pin } = request.query; 
-    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins${pin}.json`)
+    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins/PIN${pin}.json`)
     .then(data => data.json())
     .then(json => response.json(json))
 }
