329f9053092999769bc0380ecd44a94f90804b19
gerard <gerard>
2023-04-25 12:40:06 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,7 +1,7 @@
 export default function handler(request, response) {
 
 
-    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/`,{
+    fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins.json`,{
         method: "GET"
     })
     .then(eljsonperocomstring => {
