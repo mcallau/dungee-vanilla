a3ae81f2f467f81fa7ccb3ae452fc205ea79d866
Gerard Falco <gerardfp@users.noreply.github.com>
2023-04-26 16:20:09 +0200

    .

--- api/damelasquestions.js
+++ api/damelasquestions.js
@@ -1,27 +1,34 @@
 export default function handler(request, response) {
 
-    const questions = 
-        [{
-            title: "Quant es u mes u?",
-            answers: [
-                "un","dos","tres,","quatre"
-            ],
-            correct: 1
-        },
-        {
-            title: "Quant es u mes dos?",
-            answers: [
-                "un","dos","tres,","quatre"
-            ],
-            correct: 2
-        },
-        {
-            title: "Quant es dos mes dos?",
-            answers: [
-                "un","dos","tres,","quatre"
-            ],
-            correct: 3
-        }];
+    const questions = {
+        "preguntas": {
+            "pregunta0" : {
+                "pregunta": "2+2?",
+                "respuesta0": "4",
+                "respuesta1": "5",
+                "respuesta2": "2",
+                "respuesta3": "1",
+                "correcta": 0
+            },
+            "pregunta1" : {
+                "pregunta": "2+7?",
+                "respuesta0": "4",
+                "respuesta1": "5",
+                "respuesta2": "9",
+                "respuesta3": "1",
+                "correcta": 2
+            },
+            "pregunta2" : {
+                "pregunta": "2+1?",
+                "respuesta0": "4",
+                "respuesta1": "3",
+                "respuesta2": "2",
+                "respuesta3": "1",
+                "correcta": 1
+            },
+        }
+    }
+        
     
     response.json(questions);
 }
